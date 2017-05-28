3d2e1804d8f88804ec568228b0caea9516e1a15e
sebastianfrey <sebastian.frey@outlook.com>
2017-05-28 13:21:45 +0200

    DetailActivity bei Aufruf mit Museum füllen

--- app/src/main/java/edu/muenchnermuseen/activities/DetailActivity.java
+++ app/src/main/java/edu/muenchnermuseen/activities/DetailActivity.java
@@ -1,8 +1,11 @@
 package edu.muenchnermuseen.activities;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.SearchView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +15,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import edu.muenchnermuseen.R;
+import edu.muenchnermuseen.entities.Category;
+import edu.muenchnermuseen.entities.Museum;
 
 public class DetailActivity extends AppCompatActivity
-    implements NavigationView.OnNavigationItemSelectedListener {
+    implements NavigationView.OnNavigationItemSelectedListener, AdapterView.OnClickListener {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +33,6 @@ public class DetailActivity extends AppCompatActivity
     setContentView(R.layout.activity_detail);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
-
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
-      }
-    });
 
     DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
     ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +42,30 @@ public class DetailActivity extends AppCompatActivity
 
     NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
     navigationView.setNavigationItemSelectedListener(this);
+
+
+    Button gotoButton = (Button) findViewById(R.id.goto_btn);
+    gotoButton.setOnClickListener(this);
+
+    Bundle b = getIntent().getExtras();
+    Museum museum = null;
+    if(b != null) {
+      museum = (Museum) b.getSerializable("museum");
+    }
+
+    if (museum != null)
+    {
+      ImageView thumb = (ImageView) findViewById(R.id.detail_thumb);
+      TextView header = (TextView) findViewById(R.id.detail_header);
+      TextView description = (TextView) findViewById(R.id.detail_description);
+
+      String resName = "museum_" + museum.getId().toString();
+      int resId = this.getResources().getIdentifier(resName, "mipmap", this.getPackageName());
+
+      thumb.setImageResource(resId);
+      header.setText(museum.getName());
+      description.setText(museum.getDescription());
+    }
   }
 
   @Override
@@ -56,7 +82,22 @@ public class DetailActivity extends AppCompatActivity
   public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.detail, menu);
+
+    // Associate searchable configuration with the SearchView
+    SearchManager searchManager =
+        (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+    SearchView searchView =
+        (SearchView) menu.findItem(R.id.action_search).getActionView();
+    searchView.setSearchableInfo(
+        searchManager.getSearchableInfo(getComponentName()));
+
     return true;
+  }
+
+
+  @Override
+  public void onClick(View v) {
+
   }
 
   @Override
@@ -67,7 +108,7 @@ public class DetailActivity extends AppCompatActivity
     int id = item.getItemId();
 
     //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
+    if (id == R.id.action_search) {
       return true;
     }
 
